0fc4407c57f8f536d5c1e757ce1abd826c1a1ac1
paul_odhiambo <paulodhiambo962@gmail.com>
2020-05-06 22:49:36 +0300

    code update

--- app/src/main/java/com/odhiambopaul/movie/di/ViewModelFactory.kt
+++ app/src/main/java/com/odhiambopaul/movie/di/ViewModelFactory.kt
@@ -2,6 +2,7 @@ package com.odhiambopaul.movie.di
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import java.io.IOException
 import java.lang.RuntimeException
 import javax.inject.Inject
 import javax.inject.Provider
@@ -9,17 +10,17 @@ import javax.inject.Singleton
 
 @Suppress("UNCHECKED_CAST")
 @Singleton
-class ViewModelFactory @Inject constructor(private val viewModelMap: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>):ViewModelProvider.Factory
-{
+class ViewModelFactory @Inject constructor(private val viewModelMap: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        val creator = viewModelMap[modelClass]?:viewModelMap.asIterable().firstOrNull{
+        val creator = viewModelMap[modelClass] ?: viewModelMap.asIterable().firstOrNull {
             modelClass.isAssignableFrom(it.key)
 
-        }?.value?:throw IllegalArgumentException("Unknown ViewModel $modelClass")
+        }?.value ?: throw IllegalArgumentException("Unknown ViewModel $modelClass")
         return try {
             creator.get() as T
-        }catch (e:Exception){
-            throw RuntimeException(e)
+        } catch (e: IOException) {
+            throw RuntimeException(e.message)
         }
     }
 }
